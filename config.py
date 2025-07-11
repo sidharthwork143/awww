6c6799358302f9eb12dd0b8c1d3e4c337c6671fc
sidharthwork143 <sidharthwork143@gmail.com>
2025-07-12 01:37:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 # Load environment variables
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")  # From @BotFather
-TMDB_API_KEY = os.getenv("TMDB_API_KEY")  # From themoviedb.org
-ADMIN_ID = int(os.getenv("ADMIN_ID"))  # Your Telegram user ID
+BOT_TOKEN = os.getenv("6007398311:AAF1s-_khhR6fQmJRRPdU0DsDaSvhEP9TH8")  # From @BotFather
+TMDB_API_KEY = os.getenv("http://www.omdbapi.com/?i=tt3896198&apikey=a956c290")  # From themoviedb.org
+ADMIN_ID = int(os.getenv("1960614875"))  # Your Telegram user ID
 MONGO_URI = os.getenv("mongodb+srv://sidharthwork143:fn6HFQ2ErnAT7ggT@cluster0.umwqqcl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # mongodb://localhost:27017 or Atlas URI
